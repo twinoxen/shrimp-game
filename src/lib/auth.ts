da014c7da330567eaa114368d9ab16490a4205de
Jonathan Werner <1854037+twinoxen@users.noreply.github.com>
2022-01-25 13:52:12 -0500

    clean up google auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,28 +5,19 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 export async function loginWithGoogle() {
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-      // const credential = GoogleAuthProvider.credentialFromResult(result);
-      // const token = credential?.accessToken;
-      // The signed-in user info.
-      // const { user } = result;
-      console.log(result);
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const credential = GoogleAuthProvider.credentialFromResult(result);
 
-    // ...
-    }).catch((error) => {
-      console.error(error);
-    // Handle Errors here.
-    //   // const errorCode = error.code;
-    //   const errorMessage = error.message;
-    //   // The email of the user's account used.
-    //   const { email } = error;
-    //   // The AuthCredential type that was used.
-    //   const credential = GoogleAuthProvider.credentialFromError(error);
-    // // ...
-    });
+    return credential;
+  } catch (err) {
+    console.error(err);
+
+    throw err;
+  }
 }
+
+loginWithGoogle();
 
 export async function logout() {
   const auth = getAuth();
