d007637f3346597233bfe8a0719c0777b46842bc
Jonathan Werner <1854037+twinoxen@users.noreply.github.com>
2022-01-25 11:32:08 -0500

    connect firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAqRAqY0EIhzWLxrZvZdhVwkAfLpsXxbFo',
+  authDomain: 'shrimp-game-31580.firebaseapp.com',
+  projectId: 'shrimp-game-31580',
+  storageBucket: 'shrimp-game-31580.appspot.com',
+  messagingSenderId: '851215866774',
+  appId: '1:851215866774:web:2663a604fa5d25829cdbb7',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
