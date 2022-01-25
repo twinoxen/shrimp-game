836a5aa62b7899594beb5cae693acc1886542afe
Jackson <Jackson.harris3@gmail.com>
2022-01-25 14:38:38 -0500

    feat: fix app and divide homepages

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,6 @@ import './App.css';
 import UserHome from './Components/UserHome';
 import LandingPage from './Components/LandingPage';
 
-function SecondPage() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>
-          That feels like an existential question, don&apos;t you
-          think?
-        </p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
-  );
-}
-
 const currentHomepage = (loggedIn: boolean): JSX.Element => {
   if (loggedIn) {
     return <UserHome />;
@@ -45,18 +14,11 @@ const currentHomepage = (loggedIn: boolean): JSX.Element => {
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   return (
-    <>
-      {/* <>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/SecondPage">test second page</Link>
-      </> */}
-      <Routes>
-        <Route path="/SecondPage" element={<SecondPage />} />
-        <Route path="about" element={<About />} />
-        <Route path="/" element={currentHomepage(loggedIn)} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/SecondPage" element={<SecondPage />} />
+      <Route path="about" element={<About />} />
+      <Route path="/" element={currentHomepage(loggedIn)} />
+    </Routes>
   );
 }
 
