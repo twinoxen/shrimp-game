98e958a7a479740490bd49ebfc4233a8409199ca
tracilevine <televine@gmail.com>
2022-01-27 04:21:49 -0500

    added footer

--- src/Components/LandingPage/index.tsx
+++ src/Components/LandingPage/index.tsx
@@ -1,4 +1,4 @@
-import { IonPage, IonContent, IonGrid, IonRow, IonCol, IonButton, IonImg, IonItem, IonLabel, IonList, IonListHeader } from '@ionic/react';
+import { IonPage, IonContent, IonGrid, IonRow, IonCol, IonButton, IonImg, IonItem, IonLabel, IonList, IonListHeader, IonFooter, IonTitle } from '@ionic/react';
 import React from 'react';
 import * as auth from '../../lib/auth';
 import Logo from '../../assets/shrimpgamepink.png';
@@ -6,48 +6,55 @@ import Logo from '../../assets/shrimpgamepink.png';
 function LandingPage() {
   return (
     <IonPage>
-      <IonRow className="ion-justify-content-center">
-        <IonImg src={Logo} alt="shrimp games logo" />
-      </IonRow>
-      <IonRow className="ion-justify-content-center">
-        <IonCol size-md={6} className="ion-justify-content-center ion-align-content center">
-          <IonRow className="ion-justify-content-center">
-            <h3>an IRL NFT scavenger hunt at Miami Hack Week for a $1,000 cash prize</h3>
-          </IonRow>
-          <IonRow className="ion-justify-content-center">
-            <h5>SEARCH | COMPETE | SPOT</h5>
-          </IonRow>
-          <IonList>
-            <IonListHeader>
-              <IonLabel>The Rules:</IonLabel>
-            </IonListHeader>
-            <IonItem>
-              <IonLabel> There are 10 SPOT locations across Miami Hack Week </IonLabel>
-            </IonItem>
-            <IonItem>
-              <IonLabel>
-                Collect at least 5 Shrimpgames SPOTs by Friday at 2pm (expo start) 
-              </IonLabel>
-            </IonItem>
-            <IonItem>
-              <IonLabel> Receive the final envelope at the MHW expo Friday at 3pm </IonLabel>
-            </IonItem>
-            <IonItem>
-              <IonLabel> 1st team to find the final SPOT will receive $1,000 grand prize </IonLabel>
-            </IonItem>
-          </IonList>
-        </IonCol>
-      </IonRow>
-      <IonContent>
+      <IonContent color="dark">
         <IonGrid>
           <IonRow className="ion-justify-content-center">
-            <IonCol size-md={6}>
+            <IonImg src={Logo} alt="shrimp games logo" />
+          </IonRow>
+          <IonRow className="ion-justify-content-center">
+            <IonCol size-md={6} className="ion-justify-content-center ion-align-content center">
+              <IonRow className="ion-justify-content-center">
+                <h3>an IRL NFT scavenger hunt at Miami Hack Week for a $1,000 cash prize</h3>
+              </IonRow>
+              <IonRow className="ion-justify-content-center">
+                <h5>SEARCH | COMPETE | SPOT</h5>
+              </IonRow>
+              <IonList>
+                <IonListHeader>
+                  <IonLabel>The Rules:</IonLabel>
+                </IonListHeader>
+                <IonItem>
+                  <IonLabel> There are 10 SPOT locations across Miami Hack Week </IonLabel>
+                </IonItem>
+                <IonItem>
+                  <IonLabel>
+                    Collect at least 5 Shrimpgames SPOTs by Friday at 2pm (expo start)
+                  </IonLabel>
+                </IonItem>
+                <IonItem>
+                  <IonLabel> Receive the final envelope at the MHW expo Friday at 3pm </IonLabel>
+                </IonItem>
+                <IonItem>
+                  <IonLabel>
+                    1st team to find the final SPOT will receive $1,000 grand prize
+                  </IonLabel>
+                </IonItem>
+              </IonList>
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-justify-content-center">
+            <IonCol size-md={6} className="ion-justify-content-center">
               <IonButton onClick={auth.loginWithGoogle} type="button">Login with google</IonButton>
               <IonButton onClick={auth.logout} type="button">Log out</IonButton>
             </IonCol>
           </IonRow>
         </IonGrid>
       </IonContent>
+      <IonFooter className="ion-no-border">
+        <IonTitle>
+          powered by SpotX
+        </IonTitle>
+      </IonFooter>
     </IonPage>
   );
 }
