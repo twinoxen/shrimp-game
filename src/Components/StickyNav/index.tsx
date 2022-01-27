72856e2fe716f80741de6eb81176cfb373f38005
David Woodward <davidwoodward@Davids-MBP-2.attlocal.net>
2022-01-26 21:01:48 -0500

    qr code page

--- src/Components/StickyNav/index.tsx
+++ src/Components/StickyNav/index.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Nav } from 'react-bootstrap';
 import { IconContext } from 'react-icons';
 import { BsPatchCheck, BsWallet } from 'react-icons/bs';
 import { BiUserCircle } from 'react-icons/bi';
@@ -26,47 +23,7 @@ function StickyNav({ toggleModal }) {
         </IonToolbar>
       </IconContext.Provider>
     </IonFooter>
-=======
-import { Container, Row, Col } from 'react-bootstrap';
-import { FaHome } from 'react-icons/fa';
-=======
-import {
-  Container, Row, Col, Navbar, Card, Nav,
-} from 'react-bootstrap';
-import { IconContext } from 'react-icons';
-import { BsPatchCheck, BsWallet } from 'react-icons/bs';
-import { BiUserCircle } from 'react-icons/bi';
->>>>>>> feat: quick modal
-
-function StickyNav({ toggleModal }) {
-  return (
-<<<<<<< HEAD
-    <Container>
-      <Row>
-        <Col>
-          <FaHome />
-        </Col>
-      </Row>
-    </Container>
->>>>>>> prep for rebase
-=======
-    <Navbar fixed="bottom" className="justify-content-center">
-      <IconContext.Provider value={{ size: '3em' }}>
-        <Nav.Item className="mx-5">
-          <BsWallet />
-        </Nav.Item>
-
-        <Nav.Item className="mx-5">
-          <BsPatchCheck onClick={() => toggleModal()} />
-        </Nav.Item>
-
-        <Nav.Item className="mx-5">
-          <BiUserCircle />
-        </Nav.Item>
-      </IconContext.Provider>
-    </Navbar>
->>>>>>> feat: quick modal
-  );
+  )
 }
 
 export default StickyNav;
--- src/Components/UserHome/index.tsx
+++ src/Components/UserHome/index.tsx
@@ -1,27 +1,21 @@
-<<<<<<< HEAD
+import React, { useState } from 'react';
 import {
   IonAccordion, IonAccordionGroup, IonApp, IonCard, IonCol, IonContent, IonGrid, IonItem, IonLabel, IonList, IonPage, IonRow,
 } from '@ionic/react';
-import React, { useState } from 'react';
-=======
-import React, { useState } from 'react';
-// import { Routes, Route, Link } from 'react-router-dom';
-import { Container, Row, Accordion, Button, Modal } from 'react-bootstrap';
-import AccordionBody from 'react-bootstrap/esm/AccordionBody';
->>>>>>> feat: quick modal
-import StickyNav from '../StickyNav';
+import { Modal } from 'react-bootstrap'
+import StickyNav from '../StickyNav'
+import  QrScanner from '../QrScanner/index'
 
 function UserHome() {
-  const [fullscreen, setFullscreen] = useState(true);
-  const [show, setShow] = useState(false);
+  const [fullscreen, setFullscreen] = useState(true)
+  const [show, setShow] = useState(false)
 
   function toggle() {
-    console.log('hello');
-    setShow(true);
+    console.log('hello')
+    setShow(true)
   }
 
   return (
-<<<<<<< HEAD
     <IonPage>
       <IonContent>
         <IonGrid>
@@ -79,44 +73,18 @@ function UserHome() {
         </IonGrid>
       </IonContent>
       <StickyNav toggleModal={toggle} />
-    </IonPage>
-=======
-    <>
-      <Container className="pt-5">
-        <Row className="justify-content-lg-center">
-          {/* Visited Houses Accordian */}
-          <Accordion defaultActiveKey="0" className="mb-5">
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>Visited Houses</Accordion.Header>
-              <AccordionBody>
-                <Button>example house</Button>
-              </AccordionBody>
-            </Accordion.Item>
-          </Accordion>
-
-          {/* Remaining Houses Accordian */}
-          <Accordion defaultActiveKey="1">
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>Remaining Houses</Accordion.Header>
-              <AccordionBody>
-                <Button variant="light" disabled>example house</Button>
-              </AccordionBody>
-            </Accordion.Item>
-          </Accordion>
-        </Row>
-      </Container>
 
       <StickyNav key={0} toggleModal={toggle.bind(this)} />
 
       <Modal show={show} fullscreen={true} onHide={() => setShow(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal</Modal.Title>
+          <Modal.Title>Spot Scan In</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Modal body content</Modal.Body>
+        <QrScanner />
+        {/* <Modal.Body>Modal body content</Modal.Body> */}
       </Modal>
-    </>
->>>>>>> feat: quick modal
-  );
+    </IonPage>
+  )
 }
 
-export default UserHome;
+export default UserHome
