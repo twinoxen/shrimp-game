d3a48aface6e87c65aaccd6aad77880803760182
David Woodward <davidwoodward@Davids-MBP-2.attlocal.net>
2022-01-26 23:05:27 -0500

    house data

--- src/houseData.js
+++ src/houseData.js
@@ -1,18 +1,63 @@
 export const houseData = {
   69: { name: 'Scan in', video: '', nft: '' },
-  1: { name: 'HFO Hackervillage', video: '', nft: '' },
-  2: { name: 'The Annex', video: '', nft: '' },
+  1: {
+    name: 'HFO Hackervillage',
+    video:
+      'https://storage.googleapis.com/landed-public/miami-hack-week/HF0NFT.mp4',
+    nft: 'https://storage.googleapis.com/landed-public/miami-hack-week/HFO.png',
+  },
+  2: {
+    name: 'The Annex',
+    video:
+      'https://storage.googleapis.com/landed-public/miami-hack-week/HQNFT.mp4',
+    nft: 'https://storage.googleapis.com/landed-public/miami-hack-week/HQ.png',
+  },
   3: {
     name: 'Shrimp House',
     video:
       'https://storage.googleapis.com/landed-public/miami-hack-week/ShrimpNFT.mp4',
-    nft: '',
+    nft: 'https://storage.googleapis.com/landed-public/miami-hack-week/shrimp.png',
   },
-  4: { name: 'Threshold', video: '', nft: '' },
-  5: { name: 'Truework', video: '', nft: '' },
-  6: { name: 'Polygon', video: '', nft: '' },
-  7: { name: 'Usage AI', video: '', nft: '' },
-  8: { name: 'La Tropical', video: '', nft: '' },
-  9: { name: 'CIC Miami', video: '', nft: '' },
-  10: { name: 'Arena', video: '', nft: '' },
+  4: {
+    name: 'Threshold',
+    video:
+      'https://storage.googleapis.com/landed-public/miami-hack-week/ThresholdNFT.mp4',
+    nft: 'https://storage.googleapis.com/landed-public/miami-hack-week/Threshhold.png',
+  },
+  5: {
+    name: 'Truework',
+    video:
+      'https://storage.googleapis.com/landed-public/miami-hack-week/TrueWorkNFT.mp4',
+    nft: 'https://storage.googleapis.com/landed-public/miami-hack-week/TrueWork.png',
+  },
+  6: {
+    name: 'Polygon',
+    video:
+      'https://storage.googleapis.com/landed-public/miami-hack-week/PolygonNFT.mp4',
+    nft: 'https://storage.googleapis.com/landed-public/miami-hack-week/Polygon.png',
+  },
+  7: {
+    name: 'Usage AI',
+    video:
+      'https://storage.googleapis.com/landed-public/miami-hack-week/USAGE.AINFT.mp4',
+    nft: 'https://storage.googleapis.com/landed-public/miami-hack-week/Usage.Ai.png',
+  },
+  8: {
+    name: 'La Tropical',
+    video:
+      'https://storage.googleapis.com/landed-public/miami-hack-week/LaTropicalNFT.mp4',
+    nft: 'https://storage.googleapis.com/landed-public/miami-hack-week/La%20Tropical.png',
+  },
+  9: {
+    name: 'CIC Miami',
+    video:
+      'https://storage.googleapis.com/landed-public/miami-hack-week/CICNFT.mp4',
+    nft: 'https://storage.googleapis.com/landed-public/miami-hack-week/CIC.png',
+  },
+  10: {
+    name: 'Arena',
+    video:
+      'https://storage.googleapis.com/landed-public/miami-hack-week/ArenaNFT.mp4',
+    nft: 'https://storage.googleapis.com/landed-public/miami-hack-week/Arena%20House%20.png',
+  },
 }
